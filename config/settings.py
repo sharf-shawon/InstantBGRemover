c32b7b575b693f3c9fc7da6f49d00d032ea269bf
Sharfuddin Shawon <sharfuddin.shawon@gmail.com>
2025-03-14 10:20:01 -0500

    update CSRF_TRUSTED_ORIGINS to include HTTP protocol and adjust image container styling in index.html

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-l)7k$j$+w9l$ff
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
-CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', 'localhost,127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = os.environ.get('DJANGO_CSRF_TRUSTED_ORIGINS', 'http://localhost,http://127.0.0.1').split(',')
 
 # CSRF_TRUSTED_ORIGINS = [
 #     'https://bgrem.shawon.me',  # Add your domain here
