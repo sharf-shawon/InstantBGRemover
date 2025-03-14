1f49d2b97c45b251d53f7e2ee11f17432e6233cf
Sharfuddin Shawon <sharfuddin.shawon@gmail.com>
2025-03-14 11:30:07 -0500

    add static file serving to urlpatterns in urls.py

--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("remover.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
